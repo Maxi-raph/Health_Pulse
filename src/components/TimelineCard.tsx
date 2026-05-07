df9511432968ba0a74ae3e7ff464d17f1130f390
Raph <onwujekwechibuzor@gmail.com>
2026-05-07 06:32:08 +0100

    working on the timeline cards

--- src/components/TimelineCard.tsx
+++ src/components/TimelineCard.tsx
@@ -9,6 +9,9 @@ type TimelineProps = {
 const TimelineCard = ({icon, title, desc, sideText}:TimelineProps) => {
     return ( <div className="rounded-lg p-3 flex flex-col gap-3 border border-gray-300">
      <p className="flex justify-end p-1 rounded-sm">{desc}</p>
+     <p className="flex justify-end p-1 rounded-sm">{icon}</p>
+     <p className="flex justify-end p-1 rounded-sm">{title}</p>
+     <p className="flex justify-end p-1 rounded-sm">{sideText}</p>
     </div> );
 }
  
